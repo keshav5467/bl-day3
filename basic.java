1a07304e6c3b997cc5e7148700c188bf929097a1
keshav <keshavgoel166@gmail.com>
2025-06-25 15:19:52 +0530

    code pushed

---
+++ basic.java
@@ -0,0 +1,15 @@
+class basic{
+    public static void main(String[]args){
+        int a=18;
+        int b=20;
+        int c=a+b;
+        int d=a*b;
+        int e=b/a;
+        int f=a%b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+}
